9dc25784d5b67cb7486d0165322a13bc83238ce1
Kristian Payne <kristianpayne1@gmail.com>
2024-10-12 23:31:56 +0100

    Use rapier JS to fire a cannon ball

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Water from "./components/Water";
 import Ship from "./components/Ship";
 import Seagull from "./components/Seagull";
 import { ParticleSystemProvider } from "./components/ParticleSystem";
+import { Physics } from "@react-three/rapier";
 
 function App() {
     const waterRef = useRef(null);
@@ -24,42 +25,44 @@ function App() {
                 camera={{ position: [0, 45, 60], fov: 10 }}
             >
                 <Suspense fallback={null}>
-                    <ParticleSystemProvider>
-                        <ambientLight intensity={3} />
-                        <directionalLight
-                            position={[0, 10, -10]}
-                            intensity={2}
-                        />
-                        <spotLight position={[0, 3, -6]} intensity={100} />
-                        <PresentationControls
-                            global
-                            zoom={0.8}
-                            snap
-                            polar={[-Math.PI / 4.5, Math.PI / 4.5]}
-                            azimuth={[-Math.PI / 1.4, Math.PI / 2]}
-                            cursor={false}
-                        >
-                            <Bounds fit clip margin={1}>
-                                <Water ref={waterRef} />
-                                <Ship
-                                    rotation={[0, -Math.PI / 4, 0]}
-                                    scale={0.3}
-                                    waterRef={waterRef}
-                                />
-                                <Seagull
-                                    position={[2.5, 5, 0]}
-                                    rotation={[0, -Math.PI / 4, 0]}
-                                    scale={0.04}
-                                />
-                            </Bounds>
-                        </PresentationControls>
-                        <ContactShadows
-                            position={[0, -3.25, 0]}
-                            scale={20}
-                            blur={3}
-                            far={4}
-                        />
-                    </ParticleSystemProvider>
+                    <Physics>
+                        <ParticleSystemProvider>
+                            <ambientLight intensity={3} />
+                            <directionalLight
+                                position={[0, 10, -10]}
+                                intensity={2}
+                            />
+                            <spotLight position={[0, 3, -6]} intensity={100} />
+                            <PresentationControls
+                                global
+                                zoom={0.8}
+                                snap
+                                polar={[-Math.PI / 4.5, Math.PI / 4.5]}
+                                azimuth={[-Math.PI / 1.4, Math.PI / 2]}
+                                cursor={false}
+                            >
+                                <Bounds fit clip margin={1}>
+                                    <Water ref={waterRef} />
+                                    <Ship
+                                        rotation={[0, -Math.PI / 4, 0]}
+                                        scale={0.3}
+                                        waterRef={waterRef}
+                                    />
+                                    <Seagull
+                                        position={[2.5, 5, 0]}
+                                        rotation={[0, -Math.PI / 4, 0]}
+                                        scale={0.04}
+                                    />
+                                </Bounds>
+                            </PresentationControls>
+                            <ContactShadows
+                                position={[0, -3.25, 0]}
+                                scale={20}
+                                blur={3}
+                                far={4}
+                            />
+                        </ParticleSystemProvider>
+                    </Physics>
                 </Suspense>
                 <color attach="background" args={["#63d2da"]} />
             </Canvas>
--- src/components/Cannon.jsx
+++ src/components/Cannon.jsx
@@ -1,13 +1,18 @@
 import { animated, easings, useSpring } from "@react-spring/three";
-import { Outlines, useCursor } from "@react-three/drei";
-import { useRef, useState } from "react";
+import { Outlines, Sphere, useCursor } from "@react-three/drei";
+import { useEffect, useRef, useState } from "react";
 import ParticleSystem from "./ParticleSystem";
 import { QuarksUtil } from "three.quarks";
+import { RigidBody } from "@react-three/rapier";
 
-function fireCannon(setAnimating, api, explosion) {
+function fireCannon(setAnimating, api, explosion, cannonBallRigidBody) {
     return async function () {
         setAnimating(true);
+
         QuarksUtil.restart(explosion);
+        cannonBallRigidBody.setTranslation({ x: -1.4, y: 1.575, z: 1.4 }, true);
+        cannonBallRigidBody.applyImpulse({ x: -0.1, y: 0.025, z: 0.1 }, true);
+
         await api.start({
             position: [0, 0, -1],
         })[0];
@@ -17,13 +22,19 @@ function fireCannon(setAnimating, api, explosion) {
             delay: 200,
             onRest: () => setAnimating(false),
         })[0];
+
+        cannonBallRigidBody.resetForces(false);
+        cannonBallRigidBody.resetTorques(false);
+        cannonBallRigidBody.sleep();
     };
 }
 
 export default function Cannon({ geometry, position, scale, map, ...props }) {
     const [hovered, setHovered] = useState(false);
     const [animating, setAnimating] = useState(false);
+
     const explosionRef = useRef();
+    const cannonBallRef = useRef();
 
     useCursor(hovered);
 
@@ -37,6 +48,11 @@ export default function Cannon({ geometry, position, scale, map, ...props }) {
         },
     }));
 
+    useEffect(() => {
+        if (!cannonBallRef.current) return;
+        cannonBallRef.current.setGravityScale(0, true);
+    }, []);
+
     return (
         <group position={position}>
             <animated.mesh
@@ -47,7 +63,12 @@ export default function Cannon({ geometry, position, scale, map, ...props }) {
                 onPointerLeave={() => setHovered(false)}
                 onPointerDown={
                     !animating &&
-                    fireCannon(setAnimating, cannonApi, explosionRef.current)
+                    fireCannon(
+                        setAnimating,
+                        cannonApi,
+                        explosionRef.current,
+                        cannonBallRef.current
+                    )
                 }
                 {...props}
             >
@@ -57,8 +78,18 @@ export default function Cannon({ geometry, position, scale, map, ...props }) {
             <ParticleSystem
                 ref={explosionRef}
                 url="./explosion.json"
-                position={[0, 1.3, 3]}
+                position={[0, 1.25, 3]}
             />
+            <RigidBody
+                ref={cannonBallRef}
+                colliders="ball"
+                position={[1000, -1000, 1000]}
+            >
+                <Sphere args={[0.25]}>
+                    <meshStandardMaterial color="gray" />
+                    <Outlines thickness={1} color="black" />
+                </Sphere>
+            </RigidBody>
         </group>
     );
 }
