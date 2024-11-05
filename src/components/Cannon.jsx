788825a7424f7f6caeac3c66117f38788b3c52a9
Kristian Payne <kristianpayne1@gmail.com>
2024-11-05 23:02:22 +0000

    Add trail to cannon ball

--- src/components/Cannon.jsx
+++ src/components/Cannon.jsx
@@ -1,16 +1,15 @@
 import { animated, easings, useSpring } from "@react-spring/three";
-import { Outlines, Sphere, useCursor } from "@react-three/drei";
+import { Outlines, useCursor } from "@react-three/drei";
 import { useEffect, useRef, useState } from "react";
 import ParticleSystem from "./ParticleSystem";
 import { QuarksUtil } from "three.quarks";
-import { RigidBody } from "@react-three/rapier";
+import CannonBall from "./CannonBall";
 
 function fireCannon(setAnimating, api, explosion, cannonBallRigidBody) {
     return async function () {
         setAnimating(true);
 
         QuarksUtil.restart(explosion);
-        cannonBallRigidBody.setTranslation({ x: -1.4, y: 1.575, z: 1.4 }, true);
         cannonBallRigidBody.applyImpulse({ x: -0.1, y: 0.025, z: 0.1 }, true);
 
         await api.start({
@@ -25,6 +24,7 @@ function fireCannon(setAnimating, api, explosion, cannonBallRigidBody) {
 
         cannonBallRigidBody.resetForces(false);
         cannonBallRigidBody.resetTorques(false);
+        cannonBallRigidBody.setTranslation({ x: -1.4, y: 1.575, z: 1.4 }, true);
         cannonBallRigidBody.sleep();
     };
 }
@@ -50,7 +50,12 @@ export default function Cannon({ geometry, position, scale, map, ...props }) {
 
     useEffect(() => {
         if (!cannonBallRef.current) return;
+        cannonBallRef.current.setTranslation(
+            { x: -1.4, y: 1.575, z: 1.4 },
+            true
+        );
         cannonBallRef.current.setGravityScale(0, true);
+        cannonBallRef.current.sleep();
     }, []);
 
     return (
@@ -80,16 +85,7 @@ export default function Cannon({ geometry, position, scale, map, ...props }) {
                 url="./explosion.json"
                 position={[0, 1.25, 3]}
             />
-            <RigidBody
-                ref={cannonBallRef}
-                colliders="ball"
-                position={[1000, -1000, 1000]}
-            >
-                <Sphere args={[0.25]}>
-                    <meshStandardMaterial color="gray" />
-                    <Outlines thickness={1} color="black" />
-                </Sphere>
-            </RigidBody>
+            <CannonBall ref={cannonBallRef} position={[1000, -1000, 1000]} />
         </group>
     );
 }
